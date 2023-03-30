8ca4543ce18efa4fe48d25602418d26c3db4d891
Hyeeein <phi498@naver.com>
2023-03-30 11:00:34 +0900

    설치파일 수정

--- lmr/recommendation/views.py
+++ lmr/recommendation/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 import numpy as np
 import pandas as pd
+from sklearn.feature_extraction import text
+from sklearn.metrics.pairwise import cosine_similarity
+
+import warnings
+warnings.filterwarnings('ignore')
+

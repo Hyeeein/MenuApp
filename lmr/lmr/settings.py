ec6e92ecb33664a868eb45eec51cf94b6592da94
ghkdrmatks <jewon1202@gmail.com>
2023-04-01 20:55:37 +0900

    DEFAULT_RENDERER_CLASSES 수정

--- lmr/lmr/settings.py
+++ lmr/lmr/settings.py
@@ -144,13 +144,11 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        #'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        #'rest_framework.authentication.SessionAuthentication',
-        #'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
+         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',

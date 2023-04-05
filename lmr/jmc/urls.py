9203dad60383eeb4238c098595778d4a603df161
Jung ui heon <ky7662@naver.com>
2023-04-05 14:55:33 +0900

    리뷰 작성, 수정, 삭제, 조회

--- lmr/jmc/urls.py
+++ lmr/jmc/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('restaurant/<int:restaurant>/review', views.getReviewByRestaurant, name="getReviewByRestaurant"),
     path('menu/<int:menu>/nutrition', views.getNutritionByMenu, name="getNutritionByMenu"),
     path('allergy', views.UserAllergyView, name="UserAllergyView"),
+    path('review/<int:id>', views.getReview), # id에 해당하는 인덱스를 가진 리뷰를 조회 url
+    path('review/user', views.getUserReview), # 사용자가 작성한 리뷰 조회 url
+    path('review/restaurant', views.getRestaurantReview), # 음식점에 해당하는 리뷰 조회 url
+    path('reviews/update/<int:id>', views.updateReview), # 리뷰 수정 url
+    path('review/delete/<int:id>', views.deleteReview), # 리뷰 삭제 url
 ]
--- lmr/jmc/views.py
+++ lmr/jmc/views.py
@@ -60,3 +60,67 @@ def UserAllergyView(request):
     
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
+
+@api_view(['GET']) # DB에 저장된 인덱스에 해당하는 리뷰를 조회
+@permission_classes([IsAuthenticated])
+def getReview(request, id):
+    try:
+        data = Review.objects.get(id=id)
+    except Review.DoesNotExist:
+        return Response(status=404)
+    serializer = ReviewSerializer(data)
+    return Response(serializer.data)
+
+@api_view(['GET']) # 사용자가 작성한 리뷰 조회
+@permission_classes([IsAuthenticated])
+def getUserReview(request):
+    tmp = Review.objects.filter(user_id=request.user.id)
+    serializer = ReviewSerializer(tmp, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET']) # 음식점이 저장되어 있는 리뷰 조회
+@permission_classes([IsAuthenticated])
+def getRestaurantReview(request):
+    tmp = Review.objects.filter(restaurant_id=request.user.id)
+    serializer = ReviewSerializer(tmp, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST']) # 리뷰 작성 로직
+@permission_classes([IsAuthenticated])
+def postReview(request):
+    if request.method == 'POST':
+        serializer = ReviewSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=400)
+    
+@api_view(['PUT']) # 리뷰 수정 로직
+@permission_classes([IsAuthenticated])
+def updateReview(request, id):
+    
+    try:
+        review = Review.objects.get(id=id, user_id=request.user.id) # 사용자의 리뷰가 여러개일수있으니 리뷰 인덱스가 있어야함
+    except Review.DoesNotExist:
+        return Response({"user error"}, status=404)
+
+    if request.method == 'PUT':
+        serializer = ReviewSerializer(review, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+@api_view(['DELETE']) # 리뷰 삭제 로직
+@permission_classes([IsAuthenticated])
+def deleteReview(request, id):
+    try:
+        review = Review.objects.get(id=id) # 사용자의 리뷰가 여러개일수있으니 리뷰 인덱스가 있어야함
+        if review.user_id != request.user.id:
+            return Response({"user error"}, status=403)
+    except Review.DoesNotExist:
+        return Response({"review not found"}, status=404)
+    
+    review.delete()
+    return Response({"delete success"}, status=204)

a5e1157124bd600888c8290e3921bbc9b61b99d5
Hyeeein <phi498@naver.com>
2023-04-28 23:59:52 +0900

    셋팅즈 수정

--- lmr/lmr/settings.py
+++ lmr/lmr/settings.py
@@ -45,8 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'accounts',
-    'recommendation', # 메뉴 추천시스템 추가
-    'django_cleanup.apps.CleanupConfig',
+    'recommendation',
 ]
 
 MIDDLEWARE = [
